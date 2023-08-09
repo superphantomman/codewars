bb3e4eb19c62ae409de78f2fa794d475af9b695f
Maksymilian Zaczek <maksymilian.zaczek2@gmail.com>
2023-08-09 09:35:50 +0200

    passed all test

--- codewars/evaluate.py
+++ codewars/evaluate.py
@@ -7,52 +7,106 @@ def calc(expression):
     
 
     inf : float = float("inf")
-    expression.replace(" ", "") 
+    ex : str = expression.replace(" ", "") 
+    ex = ex.replace("--", "+")
+    ex = ex.replace("++", "+")
+    ex = ex.replace("+-", "-")
+    ex = ex.replace("-+", "-")
+
     expression+="+"
 
-    a : float = inf
+    buff1 : float = inf; buff2 : float = inf
     result : float = 0
 
     i = 0 
-    n: int = len( expression )
+    n: int = len( ex )
     op : str = '+'
+    op2 = ""
     ops : set = set(['+', '-', '/', '*'])
     
 #Correct multiplication problem
+    print (f"ex={ex}")
 
     while i <  n:
-        if expression[i] in ops:
-            match op:
-                case "+":
-                    print(f"{result}+={a}")
-                    if a != inf: result += a
-                    a = inf
-                case "-":
-                    print(f"{result}-={a}")
-                    if a != inf: result -= a
-                    a = inf
-                case '/':
-                    a /= float(d_str)
-                case '*':
-                    a *= float(d_str)
-            i+=1
-  
-
-        d_str = ""
-        while i < n and expression[i] not in ops:
-            d_str += expression[i]
-            i+=1
-        if a == inf and d_str!="": 
-            a = float(d_str)
- 
+        if ex[i] not in ops:
             
 
+            
+            #dealing with brackets in string
+            if(ex[i] == '('):
+                sub_ex : str=""
+                depth : int = 1
+                i+=1
+                while depth != 0:
 
 
-         
+                    print(f"{depth}->{sub_ex}")
+                    if ex[i] == '(': depth+=1
+                    elif ex[i] == ')': depth-=1
+                        
+                    
+                    sub_ex += ex[i] 
+                    
+                    i+=1
+                
+
+                d=calc(sub_ex[:-1])
+            #dealing with numbers from string
+            else:    
+                d = ""
+                while i < n and ex[i] not in ops: 
+                    d+=ex[i] 
+                    i+=1
+
+                if d == "": continue
 
 
 
 
+            if buff1 == inf: buff1 = float( d )
+            else: buff2 = float( d )
+            continue
+        
+        if op2 != "":
+            match op2:
+                case "*":
+                    # print(f"{buff1}*={buff2}") 
+                    buff1*=buff2
+                case "/": 
+                    # print(f"{buff1}/={buff2}") 
+                    buff1/=buff2
+                    
+            op2=""
+            buff2==inf
+
+        if ex[i] == '+' or ex[i] == '-':
+            match op:
+                case '+':
+                    # print(f"{result}+={buff1}")
+                    result += buff1
+                    
+                case '-':
+                    # print(f"{result}-={buff1}")
+                    result -= buff1
+            buff1 = inf
+            buff2 = inf
+            op = ex[i]
+            op2 = ''
+        else:
+            op2 = ex[i]
+        i+=1
+        
+            
+    if op2 != "":
+        match op2:
+            case "*": buff1*=buff2
+            case "/": buff1/=buff2
+
+    # print(f"buff1={buff1}")
+    match op:
+        case '+': result+=buff1
+        case '-': result-=buff1    
+
+    print(f"result:{result}")
 
     return result
--- codewars/main.py
+++ codewars/main.py
@@ -1,9 +1,13 @@
 from evaluate import calc
 
+#write more tests and nested part do it and test --
 if __name__ == "__main__":
-    tests = ["1+2*3*4-1"]
+    tests = [("3*4-1",11), ("3*4-12",0),("1*2--3*1",5), ("2+2", 4), ("3*4", 12),
+             ("( 24/( 3*4 )*( 2+2 )--1 )+1",10)
+             ]
+
     for test in tests:
-        print(f"{test}->{calc(test)}")
+        assert calc ( test[0] ) == test[1]
 
 
 
